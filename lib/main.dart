b8c284a63129d25a227e7d8f2bf176bb0045d026
Devincf <devinc.firat@gmail.com>
2021-04-18 16:57:57 +0200

    :((

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Epic Text'),
+      home: MyHomePage(title: 'Noch Epischer Text'),
     );
   }
 }
