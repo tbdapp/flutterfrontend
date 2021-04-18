b75b1461a5e33b63bc499f471bb3f025e5b734a4
xSolem <82765109+xSolem@users.noreply.github.com>
2021-04-18 17:05:27 +0200

    Text angepasst.

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Noch Epischer Text'),
+      home: MyHomePage(title: 'Noch Epischer Text2'),
     );
   }
 }
